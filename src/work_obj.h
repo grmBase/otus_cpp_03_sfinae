cc8027c986749b6048bd93dd72294d1ed73d8404
Malyshev Denis <grmbase@yandex.ru>
2021-12-12 11:32:47 +0300

    переделал из is_arithmetic в is_integral - так более правильно, вытащил отдельный хидер в include, добивил больше коментариев

--- src/work_obj.h
+++ src/work_obj.h
@@ -1,74 +1,12 @@
 //---------------------------------------------------------------------------
-#ifndef __work_obj_h__
-#define __work_obj_h__
-//---------------------------------------------------------------------------
-#include <string>
-#include <vector>
-#include <list>
+#pragma once
 //---------------------------------------------------------------------------
 
 
 /*!
-    Template for all arithmetic number. Uses is_arithmetic
+    Класс "заготовка" на случай дальнейшего оформления кода в классы.
+  По сути, просто чтобы не мусорить в main.cpp
 */
-template<typename T>
-typename std::enable_if<std::is_arithmetic<T>::value, void>::type
-print(T aArg) {
-
-  // some debug
-  //std::cout << "in print() for digitals, arg: " << aArg << std::endl;
-
-  T tmp = aArg;
-  for (size_t i = 0; i < sizeof(tmp); ++i) {
-    uint8_t tmp2 = *((uint8_t*)(&tmp) + sizeof(tmp) - i - 1);
-    std::cout << (int)tmp2;
-    if (i != sizeof(aArg) - 1) {
-      std::cout << '.';
-    }
-  }
-
-  std::cout << std::endl;
-
-}
-
-
-
-template<typename T>
-typename std::enable_if< std::is_same < T, std::string>::value, void>::type
-print(T aArg) {
-  // std::cout << "in print for string()" << std::endl;
-  std::cout << aArg << std::endl;
-}
-
-
-template<typename T>
-typename std::enable_if<
-  std::is_same <T, std::vector<uint8_t>>::value ||
-  std::is_same <T, std::list<uint8_t>>::value
-, void>::type
-
-print(T aArg) {
-
-  //std::cout << "in print for vector(), arg: " << aArg << std::endl;
-
-  bool f_is_it_first = true;
-
-  for(const auto& curr_item : aArg) {
-
-    if(!f_is_it_first) {
-      std::cout << '.';
-    }
-    else {
-      f_is_it_first = false;
-    }
-
-    std::cout << (int)curr_item;
-
-  }
-  std::cout << std::endl;
-}
-
-
 
 class t_work_obj
 {
@@ -77,5 +15,3 @@ class t_work_obj
     int func();
 };
 //---------------------------------------------------------------------------
-
-#endif
